7983764f56b8ecd7c30f300e515fba9f71f561e6
Chet Luther <chet.luther@gmail.com>
2016-01-19 14:39:49 -0500

    Switch license from GPL to Apache 2.

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
-##############################################################################
+# Copyright 2010 Chet Luther <chet.luther@gmail.com>
 #
-# Copyright (C) 2010, Chet Luther <chet.luther@gmail.com>
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from setuptools import setup, find_packages
 
-version = '1.0.3'
+version = '1.0.4'
 
 setup(
     name='snmposter',
@@ -37,7 +32,7 @@ setup(
         'Intended Audience :: Information Technology',
         'Intended Audience :: System Administrators',
         'Intended Audience :: Telecommunications Industry',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
@@ -54,7 +49,7 @@ setup(
     author='Chet Luther',
     author_email='chet.luther@gmail.com',
     url='http://github.com/cluther/snmposter',
-    license='GPLv3',
+    license='Apache 2',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     include_package_data=True,
     zip_safe=False,
